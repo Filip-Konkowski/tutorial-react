f7bb40dc33d2a0ff069b0196b712b5386ec9b6a2
Filip Konkowski <fkonkowski@gmail.com>
2022-02-13 19:58:19 +0000

    Add one component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='App'>
+      <h1>Caat are usa</h1>
+      <p>That is cat world full of wonders and fluffy staff</p>
+      <button>Super cat</button>
+    {console.log('test')}
+    </div>)
 }
 
 export default App;
