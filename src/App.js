2a4d13fcaa1efeb3cf01b7bd47d2952f793a4b19
Filip Konkowski <fkonkowski@gmail.com>
2022-02-16 06:31:22 +0000

    First use of axios

--- src/App.js
+++ src/App.js
@@ -12,10 +12,22 @@ function App() {
   useEffect(() => {
     // we add what we want to happen after rendering
     // fetch data from DB by API
-  })
+    fetchData()
+  },[])
+
+  const fetchData = async (city) => {
+    const APIKEY = '13ed397fbc9f57836cdabea81586c2c3'
+    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'London'}&appid=${APIKEY}`)
+  
+    await setAllData({
+      city:result.data.name,
+      country:result.data.sys.country,
+      temperature:result.data.main.temp
+    })
+  }
   return (
     <div className='App'>
-
+      {console.log('test', allData.country)}
     </div>)
 }
 
